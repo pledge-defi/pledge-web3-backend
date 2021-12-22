09dd06cd63d96f06c4a89371a1239d485ed18ed5
root <root@ip-172-31-45-109.us-west-2.compute.internal>
2021-12-22 08:02:51 +0000

    debtTokenList add error handler

--- app/controller/debttoken.js
+++ app/controller/debttoken.js
@@ -2,13 +2,35 @@
 
 const Controller = require('egg').Controller;
 
+const DEBT_SUCCESS = 200;
+const DEBT_ERROR   = 400;
+
+const MESSAGE_NOT_FOUND = "获取失败";
+const MESSAGE_FOUND = "获取成功";
+
 class DebtTokenController extends Controller {
     async debtTokenList() {
       const { ctx, service } = this;
 
-      const symbols = await service.debttoken.debtTokenList();
+      const symbols = await service.debttoken.debtTokenList(); 
+      if(!symbols) {
+        const body = {
+          code: DEBT_ERROR,
+          message: MESSAGE_NOT_FOUND,
+        };
 
-      ctx.body = symbols;
+        ctx.body = body;
+
+        return;
+      }
+
+    const body = {
+      code: DEBT_SUCCESS,
+      message: MESSAGE_FOUND,
+      data: symbols,
+    };
+
+      ctx.body = body;
     }
 }
 
