51bbfcce9e4296bfc6bf3714a270afc2161d2421
zTgx <747674262@qq.com>
2021-12-23 13:51:56 +0800

    add config.prod

---
+++ config/config.prod.js
@@ -0,0 +1,55 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1639718629447_8516';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+ config.cluster = {
+    listen: {
+      path: '',
+      port: 7002,
+      hostname: '0.0.0.0',
+    },
+	};
+  config.security = {
+    xframe: {
+      enable: false,
+    },
+    csrf: {
+      enable: false,
+      ignoreJSON: false
+    },
+  };
+  config.sequelize = {
+    username: 'root',
+    password: '',
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: '9521',
+    database: 'pledge',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
