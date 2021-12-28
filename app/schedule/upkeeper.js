3b77f5f05430eef98123dba5d1ded131c6ee3f3b
zTgx <747674262@qq.com>
2021-12-28 10:08:10 +0800

    add schedule upkeeper

--- app/schedule/upkeeper.js
+++ app/schedule/upkeeper.js
@@ -1,12 +1,19 @@
+/**
+ bridge 定时脚本：
+ - 每周日晚上12:00发起一次
+ */
+
 module.exports = {
   schedule: {
-    interval: '1m', // 1 分钟间隔
     type: 'all', // 指定所有的 worker 都需要执行
-  },
+    cron: '0 0 * * * 7' // 周日午夜0点触发
+  }, 
   async task(ctx) {
     // const res = await ctx.curl('http://www.api.com/cache', {
     //   dataType: 'json',
     // });
     // ctx.app.cache = res.data;
+
+    
   },
 };
