b79a826f6771146cdd4102291c7ee194b25e4952
zTgx <747674262@qq.com>
2022-01-25 19:16:35 +0800

    search & poolList add chainID

--- app/controller/contract.js
+++ app/controller/contract.js
@@ -9,23 +9,54 @@ const SEARCH_SUCCESS = 200;
 const MESSAGE_FOUND = "查询成功";
 const MESSAGE_POOL_LIST_SUCCESS = "获取PoolList成功";
 const MESSAGE_NOT_FOUND = "查询失败";
+const MESSAGE_INVALID_CHAINID = "无效的chainID";
 
 class ContracteController extends Controller {
   async poolList() {
     const { ctx, service } = this;
+    const { chainID } = ctx.request.body;
 
+    // 无效的chainID
+    if (chainID != 97 || chainID != 56) {
+      const body = {
+        code: SEARCH_ERROR,
+        message: MESSAGE_INVALID_CHAINID,
+      };
+
+      ctx.body = body;
+
+      return;
+    }
+
+    if (chainID == 97) {
+      await self.poolListTestnet();
+    }
+
+    if (chainID == 56) {
+      await self.poolListMainnet();
+    }
+
+    const body = {
+      code: SEARCH_SUCCESS,
+      message: MESSAGE_POOL_LIST_SUCCESS,
+    };
+
+    ctx.body = body;
+  }
+
+  async poolListTestnet() {
     const web3 = this.app.web3;
     const poolLength = await service.contract.length();
-	  console.log('pool length: ', poolLength);
+    console.log('pool length: ', poolLength);
 
     // 当前DB中的poolIndex
-    let current_index = await this.ctx.model.PoolBase.findOne({
+    let current_index = await this.ctx.model.testnet.PoolBase.findOne({
       order: [['pool_id', 'DESC']],
       attributes: ["pool_id"],
       limit: 1,
     });
     // console.log('current index:', current_index.id);
-	
+  
     let index = 0;   
     if (current_index != null) {
       // index = current_index.id;
@@ -43,32 +74,67 @@ class ContracteController extends Controller {
       };
       const baseData = Object.assign(baseInfo, pool_id);
       console.log('base info and id:', baseInfo);
-      const createdBaseInfo = await this.ctx.model.PoolBase.create(baseInfo);
+      const createdBaseInfo = await this.ctx.model.testnet.PoolBase.create(baseInfo);
       // console.log("baseInfo: ", createdBaseInfo);
       const dataAttributes = {
         // pooldatum_id: createdBaseInfo.id,
         pooldatum_id: createdBaseInfo.pool_id,
       };
       const data = Object.assign(dataInfo, dataAttributes);
-	    // console.log('dataInfo :', dataInfo);
-      await this.ctx.model.PoolData.create(data);
+      // console.log('dataInfo :', dataInfo);
+      await this.ctx.model.testnet.PoolData.create(data);
+    } 
+  }
+
+  async poolListMainnet() {
+    const web3 = this.app.web3;
+    const poolLength = await service.contract.length();
+    console.log('pool length: ', poolLength);
+
+    // 当前DB中的poolIndex
+    let current_index = await this.ctx.model.mainnet.PoolBase.findOne({
+      order: [['pool_id', 'DESC']],
+      attributes: ["pool_id"],
+      limit: 1,
+    });
+    // console.log('current index:', current_index.id);
+  
+    let index = 0;   
+    if (current_index != null) {
+      // index = current_index.id;
+      index = current_index.pool_id;
     }
+    for (; index < poolLength; index ++) { 
+      const baseInfo = await service.contract.baseInfo(index);
+      let dataInfo = await service.contract.dataInfo(index);
+      // const poolInfo = Object.assign(baseInfo, dataInfo);
+      // poolInfos.push(poolInfo);
 
-    const body = {
-      code: SEARCH_SUCCESS,
-      message: MESSAGE_POOL_LIST_SUCCESS,
-    };
-
-    ctx.body = body;
+      // write to db
+      const pool_id = {
+        pool_id: index+1,
+      };
+      const baseData = Object.assign(baseInfo, pool_id);
+      console.log('base info and id:', baseInfo);
+      const createdBaseInfo = await this.ctx.model.mainnet.PoolBase.create(baseInfo);
+      // console.log("baseInfo: ", createdBaseInfo);
+      const dataAttributes = {
+        // pooldatum_id: createdBaseInfo.id,
+        pooldatum_id: createdBaseInfo.pool_id,
+      };
+      const data = Object.assign(dataInfo, dataAttributes);
+      // console.log('dataInfo :', dataInfo);
+      await this.ctx.model.mainnet.PoolData.create(data);
+    } 
   }
 
   // 查询 pool
   async search() {
     const { ctx, service } = this;
-    const { poolID, state, page, pageSize } = ctx.request.body;
+    const { chainID, poolID, state, page, pageSize } = ctx.request.body;
 	  console.log('search requst: ', ctx.request.body);
 
-    const result = await ctx.service.pledgepool.search(poolID, state, page, pageSize);
+    const result = await ctx.service.pledgepool.search(chainID, poolID, state, page, pageSize);
     if(!result) {
         const body = {
           code: SEARCH_ERROR,
--- app/service/pledgepool.js
+++ app/service/pledgepool.js
@@ -1,7 +1,8 @@
 const Service = require('egg').Service;
 
 class PledgePoolService extends Service {
-    async search(poolID, state, start=1, step=10) {
+    // 默认chainID 97, BSC testnet
+    async search(chainID=97, poolID, state, start=1, step=10) {
         const curPage = start - 1;
         const pageSize = step;
         const offset = curPage * pageSize;
@@ -16,11 +17,18 @@ class PledgePoolService extends Service {
           Object.assign(condition, { state: state });
         }
         const Op = this.app.Sequelize.Op;
-        const poolInfo = await this.ctx.model.PoolBase.findAndCountAll({
+
+        let modelIns = this.ctx.model.testnet;
+        if (chainID == 56) {
+          modelIns = this.ctx.model.mainnet;
+        }
+
+        // const poolInfo = await this.ctx.model.PoolBase.findAndCountAll({
+        const poolInfo = await modelIns.PoolBase.findAndCountAll({
             attributes: ["pool_id", "settleTime", "endTime","interestRate","maxSupply","lendSupply","borrowSupply","martgageRate","lendToken","borrowToken","state","spCoin","jpCoin","autoLiquidateThreshold"],
             include: {
               attributes: ["settleAmountLend", "settleAmountBorrow", "finishAmountLend", "finishAmountBorrow", "liquidationAmounLend", "liquidationAmounBorrow"],
-              model: this.ctx.model.PoolData,
+              model: modelIns.PoolData,
               as: 'pooldata',
             },
             // where: {
