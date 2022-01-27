0eab7ae46e9dbba8b34d116ec75a2d70f6ec7a03
zTgx <747674262@qq.com>
2022-01-27 11:04:52 +0800

    fix debtTokenList 500

--- app/controller/debttoken.js
+++ app/controller/debttoken.js
@@ -7,12 +7,26 @@ const DEBT_ERROR   = 400;
 
 const MESSAGE_NOT_FOUND = "获取失败";
 const MESSAGE_FOUND = "获取成功";
+const MESSAGE_INVALID_CHAINID = "无效的chainID";
 
 class DebtTokenController extends Controller {
     async debtTokenList() {
       const { ctx, service } = this;
+      const { chainID } = ctx.request.body;
+      
+      // 无效的chainID
+      if (chainID != 97 && chainID != 56) {
+        const body = {
+          code: DEBT_ERROR,
+          message: MESSAGE_INVALID_CHAINID,
+        };
 
-      const symbols = await service.debttoken.debtTokenList(); 
+        ctx.body = body;
+
+        return;
+      }
+
+      const symbols = await service.debttoken.debtTokenList(chainID); 
       if(!symbols) {
         const body = {
           code: DEBT_ERROR,
--- app/service/debttoken.js
+++ app/service/debttoken.js
@@ -2,17 +2,28 @@
 const Service = require('egg').Service;
 
 class DebtTokenService extends Service {
-  async debtTokenList() {
+  // 默认testnet
+  async debtTokenList(chainID=97) {
     // return await this.app.debtTokenContract.methods.symbol().call().then((result) => {
     // const token_symbol = {"symbol": result};
     //       return token_symbol;
     //   });
 
-    const Op = this.app.Sequelize.Op;
-    const symbols = await this.ctx.model.DebtToken.findAndCountAll({
-      attributes: ["symbol", "address"],
-    });
-    return symbols;
+    // const Op = this.app.Sequelize.Op;
+    if (chainID == 97) {
+      const symbols = await this.ctx.model.testnet.DebtToken.findAndCountAll({
+        attributes: ["symbol", "address"],
+      });
+      return symbols;  
+    }
+    else if (chainID == 56) {
+      const symbols = await this.ctx.model.mainnet.DebtToken.findAndCountAll({
+        attributes: ["symbol", "address"],
+      });
+      return symbols;  
+    }
+
+    return null;
   }
 }
 
